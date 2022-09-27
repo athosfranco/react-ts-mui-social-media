c96bf73def9ab3bb0ad812acac3181f456923edf
Athos Franco <athos.franco123@gmail.com>
2022-09-26 21:01:07 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Typography variant="h1">Hello, World!</Typography>
+      <Button variant="contained">Hello!</Button>
+    </div>
+  );
+};
+
+export default App;
